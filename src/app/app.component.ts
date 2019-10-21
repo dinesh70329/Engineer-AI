cc008833e86ac204ea6e4a58ba4e115976602df3
sunil.mukta@ojas-it.com <pcdinesh@corp.ojas-it.com>
2019-10-21 12:34:43 +0530

    add

---
+++ src/app/app.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { Subscription, timer } from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import { ServiceService } from './service.service';
+import {switchMap} from 'rxjs/operators';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'myapp';
+  set;
+  data;
+  th;
+   subscrition:Subscription;
+   statusText:string;
+userFilter:any={title:''};
+  // url: any;
+  // id: any;
+  // created_at: any;
+  // author: any;
+  constructor(private ht:HttpClient,private a:ServiceService){
+
+  }
+  ngOnInit(){
+  this.subscrition=timer(0,1000).pipe(
+    switchMap(()=> this.a.getInfo())
+    ).subscribe(resp=>{console.log(resp)
+    this.set=resp
+    this.data=this.set.hits});
+   ;
+  }
+  // button(id){
+  //   console.log(id)
+  //  this.th=id;
+  // }
+s;
+  fun(d){
+    // this.th = d;
+    // this.url=d.url,
+    // this.title=d.title,
+    // this.author=d.author,
+    // this.created_at=d.created_at
+    console.log(d);
+    this.s=d;
+    this.a.postInfo(d).subscribe(resp=>
+      console.log(d));
+
+  }
+public selectedName:any;
+public highlightRow(d){
+  this.selectedName=d.title;
+}
+}
